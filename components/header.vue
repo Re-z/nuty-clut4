d104f3ea8473e64959c58fba679cb0884739d1b6
re-z <re-z@ukr.net>
2020-06-21 15:27:46 +0300

    initial

---
+++ components/header.vue
@@ -0,0 +1,14 @@
+<template>
+	<header class="header">
+		<div class="header__logo">
+			11
+		</div>
+		<div class="header__tel"></div>
+	</header>
+</template>
+<style lang="sass">
+.header
+	background: red
+	height: 100px
+	width: 100px
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="main-wrap">
+		<Heading></Heading>
+	</div>
+</template>
+
+<script>
+
+import Heading from '../components/header';
+
+export default {
+	components: {
+		Heading
+	}
+}
+</script>
