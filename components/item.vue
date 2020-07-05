abe8671aab703327d97fe4c770d8c13f9d2e3caa
re-z <re-z@ukr.net>
2020-07-06 00:07:25 +0300

    items section done

--- components/item.vue
+++ components/item.vue
@@ -2,18 +2,18 @@
 	<div class="item">
 		<div class="item__img-wrap">
 			<div class="item__img-prev"></div>
-			<img class="item__img" src="https://kengyry.com.ua/image/cache/catalog/fa-1342-3md/389374.970x0-262x334.jpg" alt="">
+			<img class="item__img" :src="itemObj.img2" alt="">
 			<div class="item__img-next"></div>
 		</div>
 			<div class="item__rows">
 				<div class="item__rows-visible">
 					<div class="item__row">
-						<div class="item__row-el">Lorem ipsum Lorem ipsum Lorem ipsum</div>
-						<div class="item__row-el text-right ">200грн</div>
+						<div class="item__row-el">{{itemObj.name}}</div>
+						<div class="item__row-el text-right"> {{itemObj.price}} </div>
 					</div>
 					<div class="item__row">
 						<div class="item__row-el">Код товара</div>
-						<div class="item__row-el text-right ">2233</div>
+						<div class="item__row-el text-right ">{{itemObj.code}}</div>
 					</div>
 					<div class="item__row">
 						<div class="item__row-el">Доступность</div>
@@ -23,22 +23,32 @@
 				<div class="item__rows-hidden">
 					<div class="item__row">
 						<div class="item__row-el">Размеры (д/в/ш) :</div>
-						<div class="item__row-el text-right">5/10/15 см</div>
+						<div class="item__row-el text-right">{{itemObj.size}}</div>
 					</div>
 
 					<div class="item__row">
 						<div class="item__row-el">Материал:</div>
-						<div class="item__row-el text-right">Натуральная кожа</div>
+						<div class="item__row-el text-right">{{itemObj.leather}}</div>
 					</div>
 					<div class="item__row">
 						<div class="item__row-el">Тип кожи:</div>
-						<div class="item__row-el text-right">Телячья</div>
+						<div class="item__row-el text-right">{{itemObj.leatherType}}</div>
 					</div>
 					<div class="item__row">
 						<div class="item__row-el">Цвет:</div>
-						<div class="item__row-el text-right">Бордовый</div>
+						<div class="item__row-el text-right">{{itemObj.color}}</div>
 					</div>
 				</div>	
 			</div>
 	</div>
 </template>
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		props: ['itemObj']
+	}
+</script>
--- components/items.vue
+++ components/items.vue
@@ -3,18 +3,27 @@
 		<div class="items__box">
 			<h2 class="section-title">Каталог товаров</h2>
 			<div  class="items__wrap">
-				<Item></Item>
-				<Item></Item>
-				<Item></Item>
-				<Item></Item>
-				<Item></Item>
-				<Item></Item>
-				<Item></Item>
-				<Item></Item>
+				<Item
+					v-for="item in itemsToPrint"
+					:key="item.id"
+					:itemObj="item"
+					>
+				</Item>
+				
 			</div>
 			<div class="items__btns">
-				<button class="items__btn btn btn_solid btn_med">Показать еще 20</button>
-				<button class="items__btn btn btn_dark btn_med">Показать все</button>
+				<button
+					@click="showMoreItems" 
+					class="items__btn btn btn_solid btn_med"
+					:class="{'is-hidden': this.itemsToPrint.length === this.allItems.length}">
+					Показать еще {{itemsLeft}} товаров
+				</button>
+				<button 
+					@click="showAllItems"
+					class="items__btn btn btn_dark btn_med"
+					:class="{'is-hidden': this.itemsToPrint.length === this.allItems.length}">
+					Показать все {{allItems.length}} товаров
+				</button>
 			</div>
 		</div>
 	</div>
@@ -26,8 +35,54 @@
 <script>
 import Item from '@/components/item.vue';
 export default {
+	data() {
+		return {
+			itemsToPrint: [], //items that will be displayed initialy in DOM,
+			numberOfPrintedItems: 15,
+			allItems: [], //all items recieved from DB 
+			itemsCounter: 0, //number items that will be displayed initialy in DOM,
+
+		}
+	},
+	computed: {
+		itemsLeft() {
+			const itemsLeftToPrint = this.allItems.length - this.itemsToPrint.length;
+			return itemsLeftToPrint < this.numberOfPrintedItems ? itemsLeftToPrint : this.numberOfPrintedItems
+		}
+	},
 	components: {
 		Item
+	},
+	methods: {
+		showMoreItems() {
+			this.itemsCounter += this.numberOfPrintedItems;
+			const updatedItemsToPrint = this.allItems.slice(0, this.itemsCounter);
+			this.itemsToPrint = updatedItemsToPrint;
+		},
+		showAllItems() {
+			this.itemsToPrint = this.allItems
+		}
+	},
+
+	mounted() {
+		const itemsNumber = 22;
+		for(let iterator = 1; iterator <= itemsNumber; iterator++) {
+			const newObj = {
+				img1: 'https://kengyry.com.ua/image/cache/catalog/fa-1342-3md/389374.970x0-262x334.jpg',
+				img2: 'https://www.placecage.com/262/334',
+				img3: 'https://www.placecage.com/262/335',
+				id: iterator,
+				price: '100грн',
+				name: 'Blablabla' + iterator,
+				code: iterator + 111,
+				size: '10/20/30',
+				color: 'red',
+				leather: 'Натуральная кожа',
+				leatherType: 'Свинная кожа'
+			};
+			this.allItems.push(newObj);
+		};
+		this.showMoreItems()
 	}
 }
 </script>
