f3fd10b980aed812e169250f7eb2bdb0ae52e736
re-z <re-z@ukr.net>
2020-08-09 16:59:23 +0300

    updated base

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,52 +1,166 @@
-import i3221 from '@/assets/img/items/322-1.jpg';
-import i3222 from '@/assets/img/items/322-2.jpg';
-import i3223 from '@/assets/img/items/322-3.jpg';
-import i3225 from '@/assets/img/items/322-5.jpg';
-import i3226 from '@/assets/img/items/322-6.jpg';
-import i341 from '@/assets/img/items/341-1.jpg';
-import i3551 from '@/assets/img/items/355-1.jpg';
-import i3111 from '@/assets/img/items/311-1.jpg';
-import i4111 from '@/assets/img/items/411-1.jpg';
-import i4221 from '@/assets/img/items/422-1.jpg';
+import i3511 from '@/assets/img/items/351-1.jpg';
+import i3512 from '@/assets/img/items/351-2.jpg';
+import i3513 from '@/assets/img/items/351-3.jpg';
+import i3514 from '@/assets/img/items/351-4.jpg';
+
+import i4052 from '@/assets/img/items/405-2.jpg';
+import i4053 from '@/assets/img/items/405-3.jpg';
+import i4054 from '@/assets/img/items/405-4.jpg';
+import i4055 from '@/assets/img/items/405-6.jpg';
+import i4056 from '@/assets/img/items/405-7.jpg';
+import i4057 from '@/assets/img/items/405-8.jpg';
+
+import i5031 from '@/assets/img/items/503-1.jpg';
+import i5032 from '@/assets/img/items/503-2.jpg';
+import i5033 from '@/assets/img/items/503-3.jpg';
+import i5034 from '@/assets/img/items/503-4.jpg';
+
+import i9111 from '@/assets/img/items/911-1.jpg';
+import i9112 from '@/assets/img/items/911-2.jpg';
+import i9113 from '@/assets/img/items/911-3.jpg';
+import i9115 from '@/assets/img/items/911-5.jpg';
+import i9116 from '@/assets/img/items/911-6.jpg';
+import i9117 from '@/assets/img/items/911-7.jpg';
+import i9118 from '@/assets/img/items/911-8.jpg';
+import i9119 from '@/assets/img/items/911-9.jpg';
+
+import i4412 from '@/assets/img/items/441-2.jpg';
+import i4413 from '@/assets/img/items/441-3.jpg';
+import i4414 from '@/assets/img/items/441-4.jpg';
+import i4416 from '@/assets/img/items/441-6.jpg';
+import i4417 from '@/assets/img/items/441-7.jpg';
+import i4418 from '@/assets/img/items/441-8.jpg';
+
+import i3013 from '@/assets/img/items/301-3.jpg';
+import i3014 from '@/assets/img/items/301-4.jpg';
+import i3015 from '@/assets/img/items/301-5.jpg';
+import i3016 from '@/assets/img/items/301-6.jpg';
+import i3017 from '@/assets/img/items/301-7.jpg';
+import i3018 from '@/assets/img/items/301-8.jpg';
+
+import i6011 from '@/assets/img/items/601-1.jpg';
+import i6012 from '@/assets/img/items/601-2.jpg';
+import i6014 from '@/assets/img/items/601-4.jpg';
 
 
+
+
+
+const bolv = 
+{
+	imgs: [i3511, i3512, i3513,i3514],
+	id: 351,
+	price: '600',
+	name: 'Бирюзовая сумочка из натуральной кожи',
+	size: '23/18/5',
+	stickSize: '125',
+	color: 'Бирюзовый',
+	leather: 'Натуральная кожа',
+	leatherType: 'Телячья кожа',
+	isPopular: false,
+	desc: 'Сумочка из натуральной кожи бирюзового цвета на металической молнии (серебро). Внутри сумки на подкладке карман на молнии. Ручка съемная из цепочки.',
+	comments: [
+		{
+			rating: '',
+			author: '',
+			time: '',
+			text: ''
+		}
+	]
+};
 const items = [
 	{
-		imgs: [i3221, i3222, i3223,i3225,i3226],
-		id: 322,
-		price: '100грн',
-		name: 'Blablabla111',
-		code: 332 + 111,
-		size: '10/20/30',
-		color: 'red',
+		imgs: [i3511, i3512, i3513,i3514],
+		id: 351,
+		price: '600',
+		name: 'Бирюзовая сумочка из натуральной кожи',
+		size: '23/18/5',
+		stickSize: '125',
+		color: 'Бирюзовый',
 		leather: 'Натуральная кожа',
-		leatherType: 'Свинная кожа',
-		comments: [
-			{
-				rating: 5,
-				author: 'Aut1',
-				time: '10/05/2020',
-				text: 'text from vuex'
-			},
-			{
-				rating: 4,
-				author: 'Aut12',
-				time: '10/05/2020',
-				text: 'text from vuex2'
-			},
-		]
+		leatherType: 'Телячья кожа',
+		isPopular: false,
+		desc: 'Сумочка из натуральной кожи бирюзового цвета на металической молнии (серебро). Внутри сумки на подкладке карман на молнии. Ручка съемная из цепочки.',
 	},
 	{
-		imgs: [i341,i3551,i3111,i3226],
-		id: 344,
-		price: '100грн',
-		name: 'Blablabla22',
-		code: 332 + 111,
-		size: '10/20/30',
-		color: 'red',
+		imgs: [i4052, i4053, i4054,i4055,i4056,i4057],
+		id: 405,
+		price: '600',
+		name: 'Сумочка "под золото"',
+		size: '22/17/3.5',
+		stickSize: '127',
+		color: 'Золотой',
 		leather: 'Натуральная кожа',
-		leatherType: 'Свинная кожа',
+		leatherType: 'Овчина',
+		isPopular: false,
+		desc: 'Сумочка стёганная из натуральной кожи "под золото" на металической молнии. Внутри сумки на подкладке карман на молнии. Ручка со вставками из металической цепочки.',
 	},
+	{
+		imgs: [i5031, i5032, i5033,i5034],
+		id: 503,
+		price: '600',
+		name: 'Чёрная замшевая сумочка с лаковым напылением',
+		size: '24/18/5',
+		stickSize: '130',
+		color: 'Черный замш',
+		leather: 'Натуральная кожа',
+		leatherType: 'Овчина',
+		isPopular: false,
+		desc: 'Сумочка из натуральной кожи чёрного цвета замшевая с лаковым напылением на молнии (спираль). Внутри сумки в подкладке карман на молнии. Ручка съёмная.',
+	},
+	{
+		imgs: [i9111, i9112, i9113,i9115,i9116,i9117,i9118,i9119],
+		id: 911,
+		price: '600',
+		name: 'Чёрная стёганная сумочка',
+		size: '24/18/4',
+		stickSize: '113',
+		color: 'Черный',
+		leather: 'Натуральная кожа',
+		leatherType: 'Овчина',
+		isPopular: false,
+		desc: 'Сумочка стёганная из натуральной кожи чёрного цвета на металической молнии (серебро). Внутри сумки в подкладке карман на молнии. Ручка съемная.',
+	},
+	{
+		imgs: [i4413, i4412, i4414,i4416,i4417,i4418],
+		id: 441,
+		price: '600',
+		name: 'Бежевая сумочка "под шанельку"',
+		size: '22/14/5',
+		stickSize: '125',
+		color: 'Бежевый',
+		leather: 'Натуральная кожа',
+		leatherType: 'Овчина',
+		isPopular: false,
+		desc: 'Сумочка стёганная из натуральной кожи (под шанельку) бежевого цвета на молнии (спираль). Внутри сумки на подкладке карман на молнии. Ручка съемная со вставками из цепочки (серебро).',
+	},
+	{
+		imgs: [i3013, i3014, i3015,i3016,i3017,i3018],
+		id: 301,
+		price: '600',
+		name: 'Тёмно коричневая сумочка с 3d теснением',
+		size: '24/18/5.5',
+		stickSize: '120',
+		color: 'Тёмно коричневый с 3d теснением',
+		leather: 'Натуральная кожа',
+		leatherType: 'Телятина',
+		isPopular: false,
+		desc: 'Сумочка из натуральной кожи тёмно коричневого цвета с 3d теснением на металической молнии. Внутри сумки на подкладке карман на молнии. Ручка съемная.',
+	},
+	{
+		imgs: [i6011, i6012, i6014],
+		id: 601,
+		price: '600',
+		name: 'Салатовая кожанная сумочка',
+		size: '24/18/6',
+		stickSize: 'Съемная ручка',
+		color: 'Салатовый',
+		leather: 'Натуральная кожа',
+		leatherType: 'Овчина',
+		isPopular: false,
+		desc: 'Сумочка из натуральной кожи салатового цвета на молнии спираль. Внутри сумки на подкладке карман на молнии. Ручка съемная.',
+	},
+	
 
 ]
 
